d5dcf2b96ed79e7dfeb7c0f18b0281ac62b6ee7d
ChristianBocanegra <christianmbp95@gmail.com>
2024-10-28 23:21:11 -0400

    init backend

---
+++ config/mongoose.js
@@ -0,0 +1,18 @@
+let config = require('./config');
+
+// Database setup
+const mongoose = require('mongoose');
+
+module.exports = function(){
+
+    mongoose.connect(config.ATLASDB);
+
+    let mongodb = mongoose.connection;
+
+    mongodb.on('error', console.error.bind(console, 'Connection Error: '));
+    mongodb.once('open', ()=>{
+        console.log("====> Connected to MongoDB.");
+    })
+
+    return mongodb;
+}
